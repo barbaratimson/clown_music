6df575572158427122f191b6c7354a41dda50844
barbaratimson <cokolek.sokol2014@mail.ru>
2023-09-21 06:16:30 +0700

    bugfix

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,82 @@
+import React, { useEffect,useState,useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Track from './Track';
+import Loader from './Loader';
+import { FaCrown } from "react-icons/fa6"
+import { RiArrowDownSFill, RiArrowDropUpFill, RiArrowUpSFill, RiPlayLine } from 'react-icons/ri';
+
+const link = process.env.REACT_APP_YMAPI_LINK
+
+const Chart = ({setCurrentPage,setActive,setPlayerFolded,setViewedPlaylist,currentPlaylist, setCurrentPlaylist, audioElem,setPrevSong, likedSongs, setLikedSongs, currentSong, setCurrentSong,isplaying,setCurrentSongs,isSongLoading,setIsSongLoading}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [feed,setFeed] = useState()
+
+    const fetchFeedPlaylists = async () => {
+        setIsLoading(true)
+          try {
+            const response = await axios.get(
+              `${link}/ya/feed`,);
+              setFeed(response.data)
+              console.log(response.data)
+              setIsLoading(false)
+          } catch (err) {
+            console.error('Ошибка при получении списка треков:', err);
+            console.log(err)
+          }
+      };
+      useEffect(()=>{
+          fetchFeedPlaylists()
+      },[])
+
+      if (isLoading) return <Loader></Loader>
+
+    return (
+      <div>
+
+         <div className='playlists-title'>Recommended</div>
+
+<div className="playlists">           
+{feed ? (feed.generatedPlaylists.map((playlist) => playlist.data.available ? (
+  <div className="playlist-card" key={playlist.data.playlistUuid} onClick={()=>{setViewedPlaylist(playlist.data);setActive(true)}}>
+  <div className="playlist-card-image">
+  {/* <div className='playlist-play-button' onClick={()=>{setCurrentPlaylist(playlist.data);setPlayerFolded(false)}}><RiPlayLine/></div> */}
+  <img src={playlist.data.ogImage ? `http://${playlist.data.ogImage.substring(0, playlist.data.ogImage.lastIndexOf('/'))}/200x200` : "https://music.yandex.ru/blocks/playlist-cover/playlist-cover_like.png"} loading = "lazy" alt=""></img>
+  </div>
+  <div className='playlist-card-info'>
+      <div className="playlist-card-desc">{playlist.data.title}</div>
+      {/* <div className="playlist-card-length">{playlist.trackCount}</div> */}
+  </div>
+</div>
+):(null))
+):(null)}  
+         </div>
+         <div className='playlist-songs-container'>
+                        {feed ? (feed.days[0].tracksToPlay.map((song) => song.available ? (
+                          <Track key={song.id} setPrevSong={setPrevSong} isplaying = {isplaying} audioElem={audioElem} song = {song} setCurrentSong={setCurrentSong} setCurrentSongs={setCurrentSongs} currentPlaylist={currentPlaylist} currentSong={currentSong} likedSongs={likedSongs} setLikedSongs={setLikedSongs} setIsSongLoading={setIsSongLoading} isSongLoading={isSongLoading}></Track>
+            ):(null)
+            )):(null)}
+        
+            </div>
+
+            <div className="playlists">           
+{feed ? (feed.days[0].events.map((playlist) =>(
+  <div className="playlist-card" key={playlist.id} onClick={()=>{setCurrentPlaylist(playlist);setPlayerFolded(false)}}>
+  <div className="playlist-card-image">
+  <div className='playlist-play-button' onClick={()=>{setCurrentPlaylist(playlist);setPlayerFolded(false)}}><RiPlayLine/></div>
+  <img src={playlist.ogImage ? `http://${playlist.ogImage.substring(0, playlist.ogImage.lastIndexOf('/'))}/200x200` : "https://music.yandex.ru/blocks/playlist-cover/playlist-cover_like.png"} loading = "lazy" alt=""></img>
+  </div>
+  <div className='playlist-card-info'>
+      <div className="playlist-card-desc">{JSON.stringify(playlist.title)}</div>
+      {/* <div className="playlist-card-length">{playlist.trackCount}</div> */}
+  </div>
+</div>
+))
+):(null)}  
+         </div>
+         </div>
+    );
+
+};
+
+export default Chart;
---
+++ frontend/src/components/ViewPlaylist.js
@@ -0,0 +1,92 @@
+import React, { useEffect,useState,useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Track from './Track';
+import Loader from './Loader';
+import { FaCrown } from "react-icons/fa6"
+import { RiArrowDownSFill, RiArrowDropUpFill, RiArrowUpSFill, RiPlayLine } from 'react-icons/ri';
+
+const link = process.env.REACT_APP_YMAPI_LINK
+
+const ViewPlaylist = ({active, setActive,setCurrentPage,setPlayerFolded,viewedPlaylist, setViewedPlaylist, currentPlaylist, setCurrentPlaylist, audioElem,setPrevSong, likedSongs, setLikedSongs, currentSong, setCurrentSong,isplaying,setCurrentSongs,isSongLoading,setIsSongLoading}) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchPlaylistSongs = async (userId,kind) => {
+        setIsLoading(true)
+            try {
+              const response = await axios.get(
+                `${link}/ya/playlist/tracks/${userId}/${kind}`,);
+                setViewedPlaylist(prev=>({...prev,tracks:response.data}))
+                setIsLoading(false)
+            } catch (err) {
+              console.error('Ошибка при получении списка треков:', err);
+              console.log(err)
+            }
+        };
+
+  
+        const fetchYaSongInfo = async (id) => {
+          try {
+            const response = await axios.get(
+              `${link}/ya/trackinfo/${id}`,);
+            return response.data
+          } catch (err) {
+            console.error('Ошибка при получении списка треков:', err);
+            console.log(err)
+          }
+      };
+  
+      
+
+    useEffect(()=>{
+        const handleFeed = async () => {
+        if (viewedPlaylist && viewedPlaylist.tracks && viewedPlaylist.generatedPlaylistType){
+          setIsLoading(true)
+         let result = await Promise.all(viewedPlaylist.tracks.map(async (track) => {
+            return await fetchYaSongInfo(track.id)
+        }));
+        setViewedPlaylist((prev)=>({...prev,tracks:result}))
+        setIsLoading(false)
+        } else if (viewedPlaylist.owner) {
+          fetchPlaylistSongs(viewedPlaylist.owner.uid,viewedPlaylist.kind)
+        }
+      }
+      handleFeed()
+      },[viewedPlaylist.uid])
+
+
+      // if (isLoading) return <Loader></Loader>
+    return (
+      <div>
+          <div className={"modal"} onClick={()=>setActive(false)}>
+            <div className='modal-close'>CLOSE</div>
+            <div className={"modal-content"} onClick={(e)=>e.stopPropagation()}>
+                {/* {console.log(viewedPlaylist)} */}
+           {viewedPlaylist? (
+                <div>
+                  <div className='artist-info-section'>
+                    <div className='main-image-wrapper'>  
+                    <div className='playlist-play-button' style={{pointerEvents:`${isLoading ? "none" : "all"}`}} onClick={()=>{setCurrentPlaylist(viewedPlaylist);setCurrentSong(viewedPlaylist.tracks[0]);setActive(false);setPlayerFolded(false)}}><RiPlayLine/></div>
+                <img className="image" src={viewedPlaylist.ogImage ? `http://${viewedPlaylist.ogImage.substring(0, viewedPlaylist.ogImage.lastIndexOf('/'))}/200x200` : ""} loading= "lazy" alt=""></img>
+                    </div>
+                <div className='artist-info'>
+                <div className='artist-name'>{viewedPlaylist.title}</div>
+                <div className='artist-genres'>{viewedPlaylist.description}</div>  
+                </div>              
+                  </div>
+                  {isLoading ? (<Loader/>) : viewedPlaylist.tracks ? (viewedPlaylist.tracks.map((song)=>(
+                                    <Track key={song.id} setCurrentPlaylist={setCurrentPlaylist} playlist={viewedPlaylist} setPrevSong={setPrevSong} isplaying = {isplaying} audioElem={audioElem} song = {song.track ? song.track : song} setCurrentSong={setCurrentSong} setCurrentSongs={setCurrentSongs} currentPlaylist={currentPlaylist} currentSong={currentSong} likedSongs={likedSongs} setLikedSongs={setLikedSongs} setIsSongLoading={setIsSongLoading} isSongLoading={isSongLoading}/>
+                ))):(null)}
+               
+            </div>
+            ):(null)}
+           
+         </div>
+         </div>
+         </div>
+         
+    );
+
+};
+
+export default ViewPlaylist;
